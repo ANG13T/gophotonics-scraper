2aee0c3a754f881b937de61997ed21b3b1b92c61
g4lxy <angelinatsuboi@proton.me>
2024-10-30 20:19:28 -0700

    feat: recolor `VIEW SUBSYSTEMS`

--- modules/display.py
+++ modules/display.py
@@ -6,12 +6,12 @@ from modules.config import OUTLINE, SUBSYSTEMS, SUBSUBSYSTEMS_URLS, SUBSUBSYSTEM
 console = Console()
 
 def display_subtopics():
-    tree = Tree("\n [bold white]🛰️  SATELLITE SUBSYSTEMS", guide_style="bold #5f00ff")
+    tree = Tree("\n [bold white]💥️  PHOTONICS CATALOG", guide_style="bold bright_black")
     for category, components in OUTLINE.items():
-        category_branch = tree.add(f"[bold #a500ff]{category}")
+        category_branch = tree.add(f"[bold steel_blue3]{category}")
 
         for component in components:
-            category_branch.add(f"[#00d7ff]{component}")
+            category_branch.add(f"[deep_sky_blue1]{component}")
 
     print(tree)
 
@@ -30,7 +30,7 @@ def get_subtopics_by_index(index):
         raise IndexError("Subsystem index out of range.")
 
 def select_subsystem():
-    print("\n[bold white]🛰️  SATELLITE SUBSYSTEMS[/bold white]")
+    print("\n[bold white]💥  PHOTONICS CATALOG[/bold white]")
     for i, category in enumerate(OUTLINE):
         print(f"[bold #a500ff]{i + 1}: {category}[/bold #a500ff]")
 
@@ -39,7 +39,7 @@ def select_subsystem():
     return int(choice) - 1
 
 def select_subsystem_topic(subsystem_index):
-    print("\n[bold white]🛰️  SUBSYSTEM TOPICS[/bold white]")
+    print("\n[bold white]💥️  CATALOG TOPICS[/bold white]")
     subsystem_name = list(OUTLINE.keys())[subsystem_index]
     subtopics = get_subtopics_by_index(subsystem_index)
     for i, category in enumerate(subtopics):
@@ -48,7 +48,7 @@ def select_subsystem_topic(subsystem_index):
     return subtopics[int(choice) - 1]
 
 def select_component(subsystem_index, components):
-    print("\n[bold white]🛰️  COMPONENTS[/bold white]")
+    print("\n[bold white]💥️  COMPONENTS[/bold white]")
     subsystem_name = list(OUTLINE.keys())[subsystem_index]
     subtopics = get_subtopics_by_index(subsystem_index)
     for i, category in enumerate(components):
